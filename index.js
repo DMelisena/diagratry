49cbb87ff3f29781ce3e52ed63765dc503ebdf2d
DMelisena <demonlordx777@gmail.com>
2024-10-10 05:17:53 +0700

    3 div results

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ let sq3 = square(2).fill('blue')
 
 // draw the diagram to `mysvg`
 draw(sq2, sq3);
+
+let mysvg3 = document.getElementById('mysvg3');
+
+// define the `draw` function, without this image can't be combined
+let draw3 = (...diagrams) => {
+    draw_to_svg(mysvg3, diagram_combine(...diagrams));
+};
+
+// build the diagram objects
+let sq4 = square(10).fill();
+let sq5 = square(2).fill('black')
+
+// draw the diagram to `mysvg`
+draw3(sq4, sq5);
