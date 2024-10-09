cd7c46ccc4e27de285da1c0566e2252ada932c1f
DMelisena <demonlordx777@gmail.com>
2024-10-10 04:57:49 +0700

    example of two data loading

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ let mysvg = document.getElementById('mysvg');
 
 let sq = square(10).fill('red');
 draw_to_svg(mysvg, sq);
+
+let mysvg2 = document.getElementById('mysvg2');
+
+// define the `draw` function
+let draw = (...diagrams) => {
+    draw_to_svg(mysvg2, diagram_combine(...diagrams));
+};
+
+// build the diagram objects
+let sq2 = square(10).fill();
+let sq3 = square(2).fill('blue')
+
+// draw the diagram to `mysvg`
+draw(sq2, sq3);
