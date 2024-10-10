56609f63cab47f943d0ae23434c65ddfaac19fad
DMelisena <demonlordx777@gmail.com>
2024-10-10 13:00:27 +0700

    folds it

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// test{{{
+
 import {image,square,line,curve,V2,draw_to_svg, diagram_combine} from 'https://cdn.jsdelivr.net/npm/diagramatics@1.5/dist/diagramatics.min.js' 
 let mysvg = document.getElementById('mysvg');
 
 let sq = square(10).fill('red');
 draw_to_svg(mysvg, sq);
+// }}}
 
+// draw{{{
 let mysvg2 = document.getElementById('mysvg2');
 
 // define the `draw` function, without this image can't be combined
@@ -17,6 +21,9 @@ let sq3 = square(2).fill('blue')
 
 // draw the diagram to `mysvg`
 draw(sq2, sq3);
+// }}}
+
+// draw 3{{{
 
 let mysvg3 = document.getElementById('mysvg3');
 
@@ -36,3 +43,4 @@ let src = "https://photon-ray.xyz/img/rlogo.jpg";
 let img = image(src,0.2,0.2);
 // draw the diagram to `mysvg`
 draw3(curv,img,l,l1);
+// }}}
